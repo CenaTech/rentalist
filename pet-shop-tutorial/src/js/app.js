c8d43d06d05fbf26441bf711dc31ace53248d193
Will Foley <william.w.foley@gmail.com>
2018-07-11 14:45:11 -0400

    added more comments

--- pet-shop-tutorial/src/js/app.js
+++ pet-shop-tutorial/src/js/app.js
@@ -24,7 +24,7 @@ App = {
   },
 
   initWeb3: function() {
-    // Is there an injected web3 instance?
+    // Is there an injected web3 instance? (ETH browsers like Mist or Chrome w/MetaMask create own web3 instances we'll use)
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
     } else {
@@ -38,11 +38,11 @@ App = {
 
   initContract: function() {
     $.getJSON('Adoption.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      // Get the necessary contract artifact file and instantiate it with truffle-contract in callback
       var AdoptionArtifact = data;
       App.contracts.Adoption = TruffleContract(AdoptionArtifact);
 
-      // Set the provider for our contract
+      // Set the provider for our contract (set in initWeb3 function)
       App.contracts.Adoption.setProvider(App.web3Provider);
 
       // Use our contract to retrieve and mark the adopted pets
@@ -61,11 +61,14 @@ App = {
 
     App.contracts.Adoption.deployed().then(function(instance) {
       adoptionInstance = instance;
-
+      // access deployed Adoption contract and call getAdopters
+      // using call() allows for reading data from the blockchain without sending a full trnasaction, thereby saving gas
       return adoptionInstance.getAdopters.call();
     }).then(function(adopters) {
       for (i = 0; i < adopters.length; i++) {
+        // adopters array of addresses; when empty each address is an empty address
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          // if pet does not have empty address, it's been adopted and mark it as such
           $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
@@ -85,15 +88,16 @@ App = {
       if (error) {
         console.log(error);
       }
-
+      // get first account address
       var account = accounts[0];
 
       App.contracts.Adoption.deployed().then(function(instance) {
         adoptionInstance = instance;
 
-        // Execute adopt as a transaction by sending account
+        // Execute adopt as a transaction by sending account from above
         return adoptionInstance.adopt(petId, {from: account});
       }).then(function(result) {
+        // transaction successful, call markAdopted to update interface
         return App.markAdopted();
       }).catch(function(err) {
         console.log(err.message);
