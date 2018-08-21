c1d64bc62af12cce000ec263548db22e837cd8d5
cenatech <joe@cenatech.com>
2018-08-21 16:01:25 -0400

    Finish front end conversion from pets to equipment rental

--- pet-shop-tutorial/src/js/app.js
+++ pet-shop-tutorial/src/js/app.js
@@ -37,39 +37,39 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('Rental.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract in callback
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      var RentalArtifact = data;
+      App.contracts.Rental = TruffleContract(RentalArtifact);
 
       // Set the provider for our contract (set in initWeb3 function)
-      App.contracts.Adoption.setProvider(App.web3Provider);
+      App.contracts.Rental.setProvider(App.web3Provider);
 
-      // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
+      // Use our contract to retrieve and mark the rented equipment
+      return App.markRented();
     });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-rent', App.handleRental);
   },
 
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
+  markRented: function(renters, account) {
+    var rentalInstance;
 
-    App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-      // access deployed Adoption contract and call getAdopters
+    App.contracts.Rental.deployed().then(function(instance) {
+      rentalInstance = instance;
+      // access deployed Rental contract and call getRenters
       // using call() allows for reading data from the blockchain without sending a full trnasaction, thereby saving gas
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
-      for (i = 0; i < adopters.length; i++) {
-        // adopters array of addresses; when empty each address is an empty address
-        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          // if pet does not have empty address, it's been adopted and mark it as such
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+      return rentalInstance.getRenters.call();
+    }).then(function(renters) {
+      for (i = 0; i < renters.length; i++) {
+        // renters array of addresses; when empty each address is an empty address
+        if (renters[i] !== '0x0000000000000000000000000000000000000000') {
+          // if equipment does not have empty address, it's been rented and mark it as such
+          $('.panel-equipment').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
     }).catch(function(err) {
@@ -77,12 +77,12 @@ App = {
     });
   },
 
-  handleAdopt: function(event) {
+  handleRental: function(event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var equipmentId = parseInt($(event.target).data('id'));
 
-    var adoptionInstance;
+    var rentalInstance;
 
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
@@ -91,14 +91,14 @@ App = {
       // get first account address
       var account = accounts[0];
 
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
+      App.contracts.Rental.deployed().then(function(instance) {
+        rentalInstance = instance;
 
-        // Execute adopt as a transaction by sending account from above
-        return adoptionInstance.adopt(petId, {from: account});
+        // Execute rent as a transaction by sending account from above
+        return rentalInstance.rent(equipmentId, {from: account});
       }).then(function(result) {
-        // transaction successful, call markAdopted to update interface
-        return App.markAdopted();
+        // transaction successful, call markRented to update interface
+        return App.markRented();
       }).catch(function(err) {
         console.log(err.message);
       });
