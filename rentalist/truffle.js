7ae5a026a2caf081d874782e0d780119edf32500
cenatech <joe@cenatech.com>
2018-08-22 22:43:04 -0400

    Rename root folder to rentalist

--- rentalist/truffle.js
+++ rentalist/truffle.js
@@ -5,7 +5,12 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: 1 // Match any network id
+    },
+    live: {
+   	  host: "127.0.0.1",
+      port: 7546,
+      network_id: 2 // Match any network id
     }
   }
 };
