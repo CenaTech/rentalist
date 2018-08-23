55671d9edf69056155652149164fc87f91d7f768
cenatech <joe@cenatech.com>
2018-08-23 00:29:56 -0400

    Deploy app to ropsten, saving via git to try to deploy front end app

--- rentalist/truffle.js
+++ rentalist/truffle.js
@@ -4,42 +4,18 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 //Instead, the environment variables should be set manually on the host machine
 //Environment variables for dotenv are stored in the ".env" file in this directory
 
-require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+require('dotenv').load();  // Store environment-specific variable from '.env' to process.env
 
 module.exports = {
   networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // Match any network id
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // matching any id
     },
-    // testnets
-    // properties
-    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
-    // gas: gas limit
-    // gasPrice: gas price in gwei
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 3,
-      gas: 3000000,
-      gasPrice: 21
-    },
-    kovan: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
-      network_id: 42,
-      gas: 3000000,
-      gasPrice: 21
-    },
-    rinkeby: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
-      network_id: 4,
-      gas: 3000000,
-      gasPrice: 21
-    },
-    // main ethereum network(mainnet)
-    main: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
-      network_id: 1,
       gas: 3000000,
       gasPrice: 21
     }
